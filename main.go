e7b6318ea16029d5bcafbbe38d97708003516822
jamie404 <jamie404notfound@gmail.com>
2025-01-20 22:44:09 +0000

    Login working

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"jamie404notfound/go-backend/controllers"
 	"jamie404notfound/go-backend/initializers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,8 +16,16 @@ func init() {
 func main() {
 	r := gin.Default()
 
+	// Configure CORS settings
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},                   // Allow requests from your Vue frontend
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},            // Allow specific HTTP methods
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Allow specific headers
+		AllowCredentials: true,                                                // Allow credentials (cookies, authorization headers, etc.)
+	}))
+
 	r.GET("/getUsers", controllers.GetUsers)
-	r.GET("/login", controllers.LoginCheck)
+	r.POST("/login", controllers.LoginCheck)
 	r.POST("/createUser", controllers.UserCreate)
 
 	r.Run() // listen and serve on 0.0.0.0:3000
